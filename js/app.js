b1c9738b78387697130e0cea5a13bf9b0431fbd2
smitzter <smittybuckler@gmail.com>
2017-06-23 01:26:50 -0700

    I made Chutulu bounce around a black screen with red blinking words that threaten the viewer bc why not... ok the assignment was to animate the page somehow.

--- js/app.js
+++ js/app.js
@@ -1,239 +1,174 @@
 'use strict';
 
-// for (var p = 0; p < productNames.length; p++)
-var productNames = ['babySweep', 'bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair','cthulhu', 'dogDuck', 'dragon', 'pen', 'petSweep', 'scissors', 'shark', 'tauntaun', 'unicorn', 'usb', 'waterCan', 'wineGlass' ]
-// }
-console.log(productNames);
-
-var productName = [];
-var productImage1 = '';
-var productImage2 = '';
-var productImage3 = '';
-
-//TODO automate var from Array
-// var arrayOfObjects = [];
-// for (var i=0; i < )
+//Inspired often by Sugey & Jon & the first 24
+///var///////////
+var product;
+var img;
+var newImg;
+var pushImg;
+var img1;
+var img2;
+var img3;
 var attempts = 0;
 var maxAttempts = 25;
-var babySweep = new ProductName('babySweep');
-var bag = 0;
-var banana = 0;
-var bathroom = 0;
-var boots = 0;
-var breakfast = 0;
-var bubblegum = 0;
-var chair = 0;
-var cthulhu = 0;
-var dogDuck = 0;
-var dragon = 0;
-var pen = 0;
-var petSweep = 0;
-var scissors = 0;
-var shark = 0;
-var tauntaun =0;
-var unicorn = 0;
-var usb = 0;
-var waterCan = 0;
-var wineGlass = 0;
-
-var productImageParent = document.getElementById('productImage');
-var responseParent = document.getElementById('response');
-
-function setup() {
-  //think about what does setup do
-  //name var = element(object)
-  productImage1 = generateRandomProduct();
-  productImage2 = generateRandomProduct();
-  productImage3 = generateRandomProduct();
-//TODO make random not equal last 3 nor other 2
-  if (attempts) {
-    productImageParent.removechild(productImageParent.lastChild);
-    productImageParent.removechild(productImageParent.lastChild);
-    productImageParent.removechild(productImageParent.lastChild);
-  }
-  // for (var p = 0; p < 3; i++) {
-  //   productNames ;
-  // }
-  renderProductImage(productImage1);
-  renderProductImage(productImage2);
-  renderProductImage(productImage3);
-  // updateAnswer();
-  // updateAttempts();
-}
-setup();
-renderResponse();
-data();
-buttonLogic();
+//get placing from website///
+var productImageParent = document.getElementById('ImageAll');
+var productImg1 = document.getElementById('img1');
+var productImg2 = document.getElementById('img2');
+var productImg3 = document.getElementById('img3');
 
 
-productImageParent.addEventListener('render', function(render) {
-  var display = event.target.getAttribute('id');
-  render(display);
-  //same logic as below? Find a way to loop this to make code dry
-});
-
-
-productImageParent.addEventListener('click', function(event) {
-  if (attempts === maxAttempts) {
-    return;
-  }
-
-  var answer = event.target.getAttribute('id');
-  attempts++;
-//for i statement - constructor
-  if (answer === [0]) {
-    babySweep++;
-  } else if (answer === [1]) {
-    bag++;
-  } else if (answer === [2]) {
-    banana++;
-  } else if (answer === [3]) {
-    bathroom++;
-  } else if (answer === [4]) {
-    boots++;
-  } else if (answer === [5]) {
-    breakfast++;
-  } else if (answer === [6]) {
-    bubblegum++;
-  } else if (answer === [7]) {
-    chair++;
-  } else if (answer === [8]) {
-    cthulhu++;
-  } else if (answer === [9]) {
-    dogDuck++;
-  } else if (answer === [10]) {
-    dragon++;
-  } else if (answer === [11]) {
-    pen++;
-  } else if (answer === [12]) {
-    petSweep++;
-  } else if (answer === [13]) {
-    scissors++;
-  } else if (answer === [14]) {
-    shark++;
-  } else if (answer === [15]) {
-    tauntaun++;
-  } else if (answer === [16]) {
-    unicorn++;
-  } else if (answer === [17]) {
-    usb++;
-  } else if (answer === [18]) {
-    waterCan++;
-  } else if (answer === [19]) {
-    wineGlass++;
-  } else {
-    alert('please click on image');
-  }
-
-  if (attempts === maxAttempts) {
-    chart();
-  }
-});
-
-function ProductName(name, click, shown) {
-  this.name = name,
-  this.click = 0,
-  this.shown = 0
-}
-
-function generateRandomProduct() {
-  var index = Math.floor(Math.random() * productNames.length);
-  return productNames[index];
-}
-// function updateProductName (productName) {
-//   productNameElement.textcontent = productName;
-// }
-// function renderProductName (productName) {
-//   var h3 = document.createElement('h3');
-//   h3.textContent = productName;
-//   productNameParent.append('h3');
-// }
-//modeled from jon
-function buttonLogic() {
-  productName = [], answer = [];
-  for (var i = 0; i < productNames.length; i++){
-    productName.push(productNames[i].name);
-    answer.push(productNames[i]).clicked;
-  }
-}
-//return logic
-function data() {
-  // document.getElementById('data');
-  for (var i = 0; i < productNames.length; i++){
-    productName.push(productNames[i].name);
-    answer.push(productNames[i]).clicked;
-  }
-}
-
-for(var i = 0; i < localStorage.length; i++){
-  console.log(localStorage.key(i));
-}
-localStorage.setItem('clicked', 'data');
-localStorage.key();
-
-for (var n = 0; n < productNames.length; n++) {
-  arrayofObjects.push(ProductName = productNames[n]);
-}
-console.log(ProductName);
-
-// for (var i = 0; i < 3; i++) {
-//   arrayofObjects.push(productImage + i = ProductName);
-// }
-function updateProductName (productName) {
-  productNameElement.textContent = productName;
-}
-
-updateProductName();
-
-function renderProductImage (productNames) {
-  var img = document.createElement('img');
-  img.setAttribute('src', 'img/' + productNames + '.jpg' );
-  img.setAttribute('id', productNames);
-  productImageParent.append(img);
-}
-console.log(productNames);
-
-// productImageOne = productImage;
-// productImageTwo = productImage;
-// productImage3 = productImage;
-
-//
-function renderResponse(response) {
-  // responseElement.textContent = response;
-  var p = document.createElement('p');
-  p.textContent = response;
-  responseParent.append(p);
-}
-
-//answers but needs to reiterate
-function updateAnswer () {
-  answerElement.textContent = answer;
-}
-updateAnswer();
-//counts down attempts to 0
-function updateAttempts () {
-  attmeptsElement.textcontent = maxAttempts - attempts;
-}
+///calls////
+renderProductImage();
+render();
 updateAttempts();
-////////////////////////////////
+countViews();
+countClicks();
+localData();
+localStorage();
+retrieveData();
+///Constuctor functions///
+//array to feed constructor function: (imageNmae, location, views, clicks) MakeImage calls at end
+function MakeImage (name, filepath, timesShown, clicks) {
+  this.name = name,
+  this.filepath = filepath,
+  this.timesShown = 0,
+  this.clicks = 0,
+  pushImg.push(this);
+}
+  //////////////////
+new MakeImage('babySweep', 'img/babySweep.jpg', 0, 0);
+new MakeImage('bag', 'img/bag.jpg', 0, 0);
+new MakeImage('bathroom', 'img/bathroom.jpg', 0, 0);
+new MakeImage('breakfast', 'img/breakfast.jpg', 0, 0),
+new MakeImage('bubblegum', 'img/bubblegum.jpg', 0, 0),
+new MakeImage('chair', 'img/chair.jpg', 0, 0),
+new MakeImage('cthulhu', 'img/cthulhu.jpg', 0, 0),
+new MakeImage('dogDuck', 'img/dogDuck.jpg', 0, 0),
+new MakeImage('dragon', 'img/dragon.jpg', 0, 0),
+new MakeImage('pen', 'img/pen.jpg', 0, 0),
+new MakeImage('petSweep', 'img/petSweep.jpg', 0, 0),
+new MakeImage('scissors', 'img/scissors.jpg', 0, 0),
+new MakeImage('shark', 'img/shark.jpg', 0, 0),
+new MakeImage('tauntaun', 'img/tauntaun.jpg', 0, 0),
+new MakeImage('unicorn', 'img/unicorn.jpg', 0, 0),
+new MakeImage('usb', 'img/usb.jpg', 0, 0),
+new MakeImage('waterCan', 'img/waterCan.jpg', 0, 0),
+new MakeImage('wineGlass', 'img/wineGlass.jpg', 0, 0);
+
+
+///functions//////////////////////
+
+//Limit Attempts///
+if (attempts === maxAttempts) {
+  chart();
+}
+//pull three from array///
+
+
+function imgPath() {
+  var ip = MakeImage.setAttribute('src');
+  ip.value = product.path;
+  img.setAttributeNode(ip);
+}
+//render with math random///
+//check for dups///
+function render() {
+  var randomImg = function() {
+    img1 = Math.floor(Math.random() * imgPath.length);
+    img2 = Math.floor(Math.random() * imgPath.length);
+    img3 = Math.floor(Math.random() * imgPath.length);
+    newImg = [];
+    pushImg.push(img1, img2, img3);
+  };
+  while ( event.target.id === img1.name || event.target.id === img2.name || event.target.id === img3.name) {
+    randomImg();
+  }
+}
+
+//put three img into placing///
+function renderProductImage (products, imgAll) {
+  var x = document.getElementById('id'); document.createAttribute('src');
+  x.value = 'filepath';
+  x.setAttributeNode(x);
+  console.log(node.getAttribute('src'));
+}
+
+
+///listener for views///
+function countViews(event) {
+  var viewImg;
+  switch (event.target.id) {
+  case'':
+    viewImg = pushImg[newImg[0]];
+    break;
+  }
+}
+//listener for clicks///
+function countClicks(event) {
+  var clickedImg;
+  switch (event.target.id) {
+  case'img1':
+    clickedImg = pushImg[newImg[0]];
+    break;
+  }
+}
+function countClicks(event) {
+  var clickedImg;
+  switch (event.target.id) {
+  case'img2':
+    clickedImg = pushImg[newImg[0]];
+    break;
+  }
+}
+function countClicks(event) {
+  var clickedImg;
+  switch (event.target.id) {
+  case'img3':
+    clickedImg = pushImg[newImg[0]];
+    break;
+  }
+}
+// somevar.ImageAll.addEventListener('clicks', someobj.somemethod);
+// somvar.displayImages();
+//countdown attempts
+function updateAttempts () {
+  attemptsElement.textcontent = maxAttempts - attempts;
+}
+
+//data localStorage///
+function localData() {
+  var dataMemo = JSON.stringify(pushImg);
+  localStorage.setItem('data', dataMemo);
+}
+function retrieveData() {
+  var dataMemo = localStorage.getItem('data');
+  if(dataString){
+    var data = JSON.parse(dataMemo);
+    pushImg = data;
+  }
+}
+//display data in chart///
+//TODO make labels for data
+//TODO one column for clicks, one column for views
 function chart () {
   var canvas = document.getElementById('chart');
   var ctx = canvas.getContext('2d');
 }
-//modeled from class
+if (attempts === maxAttempts) {
+  chart();
+}
+
 var chart = new Chart(ctx, {
   type: 'bar',
-
   data: {
-    labels: ['answer', 'attempts'],
+    labels: ['babySweep', 'bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair','cthulhu', 'dogDuck', 'dragon', 'pen', 'petSweep', 'scissors', 'shark', 'tauntaun', 'unicorn', 'usb', 'waterCan', 'wineGlass'],
     datasets: [{
-      label: '# of correct answers',
+      label: ['# of clicks', '# of views'],
       backgroundColor: 'rgb(2, 98, 252)',
-      borderColor: '000f28',
-      data: [score, maxAttempts],
+      borderColor: '#000f28',
     }]
   },
-
   options: {
     scales: {
       yAxes: [{
@@ -243,8 +178,7 @@ var chart = new Chart(ctx, {
       }]
     }
   }
-});
-
+},
 function draw() {
   var canvas = document.getElementById('chart');
   var ctx = canvas.getContext('2d');
@@ -256,5 +190,4 @@ function draw() {
   ctx.fillRec(80, 10, 20, 100);
 
   ctx.fillText('Words', 10, 100);
-}
-draw();
+});
